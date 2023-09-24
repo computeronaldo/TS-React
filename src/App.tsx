037d4a194253096863481522ffaa70b8a72ee9f2
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2023-09-25 00:33:05 +0530

    working with react hooks in TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
-import Heading from "./components/Heading";
-import Section from "./components/Section";
-import Counter from "./components/Counter";
-import List from "./components/List";
+import { useState, useEffect, useCallback, useMemo, useRef } from "react";
 
-import { useState } from "react";
-function App() {
-  const [count, setCount] = useState<number>(1);
+interface User {
+  id: number;
+  username: string;
+}
+
+type fibFunc = (n: number) => number
+
+const fib: fibFunc = (n) => {
+  if(n < 2) {
+    return n;
+  } 
+  return fib(n - 1) + fib(n - 2);
+}
+
+const myNum: number = 37;
+
+const App = () => {
+  const [count, setCount] = useState<number>(0);
+  const [users, setUsers] = useState<User[] | null>(null);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  console.log(inputRef?.current);
+  console.log(inputRef?.current?.value);
+
+  useEffect(() => {
+    console.log("mounting");
+    console.log("Users:", users);
+
+    return () => {
+      console.log("Unmounting");
+    };
+  }, [users]);
+
+  const addTwo = useCallback((): void => {
+    setCount((prev) => prev + 2);
+  }, []);
+
+  const result = useMemo<number>(() => fib(myNum), [myNum]);
 
   return (
-    <>
-      <Heading title="Hello" />
-      <Section title="Different title">This is my section.</Section>
-      <Counter setCount={setCount}>Count is: {count}</Counter>
-      <List
-        items={["☕ Coffee", "🌮 Tacos", "🧑🏽‍💻 Code"]}
-        render={(item: string) => {
-          return <span className="bold">{item}</span>;
-        }}
-      />
-    </>
+    <div className="App">
+      <h1>{count}</h1>
+      <h2>{result}</h2>
+      <button onClick={addTwo}>Add</button>
+      <input ref={inputRef} type="text"/>
+    </div>
   );
-}
+};
 
 export default App;
